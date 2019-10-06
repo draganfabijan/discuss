53f1848c23f450e8fcf72afd50f231ab07bcc017
draganfabijan <dragan.fabijan@gmail.com>
2019-10-06 23:36:22 +1100

    Added option for user to sign out or sign in

---
+++ web/controllers/plugs/set_user.ex
@@ -0,0 +1,21 @@
+defmodule Discuss.Plugs.SetUser do
+  import Plug.Conn
+  import Phoenix.Controller
+
+  alias Discuss.Repo
+  alias Discuss.User
+
+  def init(_params) do # called only 1 time
+  end
+
+  def call(conn, _params) do
+    user_id = get_session(conn, :user_id)
+
+    cond do
+      user = user_id && Repo.get(User, user_id) ->
+        assign(conn, :user, user)
+      true ->
+        assign(conn, :user_id, nil)
+    end
+  end
+end
