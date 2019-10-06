1c9ea9d3c68a4acb74caeb66b9d0d136385a5d03
draganfabijan <dragan.fabijan@gmail.com>
2019-10-06 22:09:18 +1100

    Added github authentication

--- web/router.ex
+++ web/router.ex
@@ -19,6 +19,13 @@ defmodule Discuss.Router do
     resources "/topics", TopicController
   end
 
+  scope "/auth", Discuss do # just a namsespace to url
+    pipe_through :browser # znaci ceo pipeline plug mora da prodje
+
+    get "/:provider", AuthController, :request
+    get "/:provider/callback", AuthController, :callback
+  end
+
   # Other scopes may use custom stacks.
   # scope "/api", Discuss do
   #   pipe_through :api
